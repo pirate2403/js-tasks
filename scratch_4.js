1e4ec04ab697eb2c2f9bb24b56077d2e2802d1ff
Anatoly Birt <65222585+Anatoly2403@users.noreply.github.com>
2021-02-27 02:45:46 +0800

    Add files via upload

--- scratch_4.js
+++ scratch_4.js
@@ -2,15 +2,16 @@ function School(name, minYears) {
     // осуществлен перевод в arrow function
     // используется camelCase
     // так как снаружи вызывается только метод WELCOME убрал this чтобы не было доступа снаружи
-
-    if (!name) { throw Error("Не указано название школы"); }
-    if (!minYears) { throw new Error("Не указано минимальное количество лет"); }
-    // убрал лишние проверки
-
     minYears = parseInt(minYears);
     schoolName = name.trim();
     // добавил методы(parseInt, trim) при присвоении переменных, убрал контекст, т.к переменнтые используются внутри функции
-
+    if (!schoolName) {
+        throw Error("Не указано название школы");
+    }
+    if (!minYears) {
+        throw Error("Не указано минимальное количество лет");
+    }
+    // убрал лишние проверки
     checkAge = (age) => {
         if (age < minYears) {
             return {
@@ -25,33 +26,33 @@ function School(name, minYears) {
         }
     };
     //используются переменные без this, исправлена интерпаляция и второй блок else if
-
-
-    getTeacherList = () => {
-        return [
-            "А. С. Иванов",
-            "В. С. Петров",
-            "И. А. Сидоров"
-        ];
-    }
+    getTeacherList = [
+        "А. С. Иванов",
+        "В. С. Петров",
+        "И. А. Сидоров"
+    ]
     // заменил return OBJ на ARR, так как используется в след методе LENGTH
-
     getTeacher = (id) => {
-        if (!id) { id = Math.floor(Math.random() * getTeacherList().length); }
-        return getTeacherList()[id];
+        if (!id) { id = Math.floor(Math.random() * getTeacherList.length); }
+        return getTeacherList[id];
     };
     // если не задавать id в ручную, то id сгенерируется автоматом
 
     //дописана функция попроверке введенных данных
     checkValue = (val, promptMes) => {
-        let data = val ? val : prompt(promptMes);
+        let data = String(val);
+        data = data ? data : prompt(promptMes);
         let count = 0;
 
-        while (!name) {
-            alert('Вы не ввели данные!');
+        if (data === null) {
+            return;
+        }
+
+        while (!data || !data.trim() || data === NaN) {
+            alert('Вы не ввели данные, повторите ввод!');
             data = prompt(promptMes);
             count++
-            if (count == 3) {
+            if (count === 1) {
                 return
             }
         }
@@ -63,20 +64,23 @@ function School(name, minYears) {
         const teacherName = getTeacher();
 
         let persone = checkValue(name, 'Как вас зовут?');
-        let personeYear = checkValue(years, 'Укажите ваш возраст')// prompt       
-
-        if (!personeYear || !persone) {
-            alert('Вы не верно указали данные. Попробуйте начать сначала.');
+        if (!persone) {
+            alert('Вы не указали имя. Попробуйте начать сначала.');
             return;
         }
 
+        let personeYear = checkValue(years, 'Укажите ваш возраст')// prompt       
+        if (!personeYear) {
+            alert('Вы не указали возраст. Попробуйте начать сначала.');
+            return;
+        }
 
         if (checkAge(personeYear).result) {
             alert(`Добро пожаловать в автошколу "${schoolName}", ${persone} \nВаш преподаватель: ${teacherName} \nЖдём вас по адресу: ${schoolAddress}`);
         } else if (!checkAge(personeYear).result) {
             return alert(`Уважаемый ${persone}, ${checkAge(personeYear).message}`);
         }
-        // Убрал лишний alert и retutn
+        // Убрал лишний alert и return
     };
     // Убрал retutn
 }
